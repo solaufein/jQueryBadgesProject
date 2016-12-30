473bb8362d7782dd7a824dde2e6a474a73fef089
Radek <radoslawwitek@gmail.com>
2016-12-30 21:45:44 +0100

    next committtt

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,39 @@
 $(function() {
+  $.ajax({
+      url: 'https://www.codeschool.com/users/2186097.json',
+      dataType: 'jsonp',
+      success: function(response) {
+        addCourses(response.courses.completed);
+      }
+    });
 
-  // your code will go here
+    function addCourses(courses) {
 
+      var $badges = $('#badges');
+
+      courses.forEach(function (course) {
+
+        console.log(course);
+
+        var $course = $('<div />', {
+          'class': 'course'
+        }).appendTo($badges);
+
+        $('<h3 />', {
+          text: course.title
+        }).appendTo($course);
+
+        $('<img />', {
+          src: course.badge
+        }).appendTo($course);
+
+        $('<a />', {
+          'class': 'btn btn-primary',
+          target: '_blank',
+          href: course.url,
+          text: 'See Course'
+        }).appendTo($course);
+
+      });
+    }
 });
